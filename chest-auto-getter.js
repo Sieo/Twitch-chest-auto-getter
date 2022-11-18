7a7a543c17aca7f360fdbb2db406c1dec22fc2be
Sieo <Cosentino.hugo@gmail.com>
2022-11-18 15:34:23 +0100

    Update chest-auto-getter.js
    ameliorations et possibilité de relancer le tout sans recharger la page

--- chest-auto-getter.js
+++ chest-auto-getter.js
@@ -12,18 +12,16 @@
 (function() {
    'use strict';
    var number_chest_got = 0;
+    const tw_chest_icon = '<div style="width: 2rem; height: 2rem; fill: var(--color-text-base); margin-right: 0.5rem !important; cursor: pointer"><div class="tw-icon"><svg width="100%" height="100%" version="1.1" viewBox="0 0 20 20" x="0px" y="0px" class="ScIconSVG-sc-1bgeryd-1 cMQeyU"><g><path fill-rule="evenodd" d="M16.503 3.257L18 7v11H2V7l1.497-3.743A2 2 0 015.354 2h9.292a2 2 0 011.857 1.257zM5.354 4h9.292l1.2 3H4.154l1.2-3zM4 9v7h12V9h-3v4H7V9H4zm7 0v2H9V9h2z" clip-rule="evenodd"></path></g></svg></div></div>';
 
     function waitForElm(selector) {
-        console.log('--------------In promise--------------');
         return new Promise(resolve => {
-            console.log("QuerySelector 1 : ", document.querySelector(selector));
             if (document.querySelector(selector)) {
                 return resolve(document.querySelector(selector));
             }
 
             const observer = new MutationObserver(mutations => {
                 if (document.querySelector(selector)) {
-                    console.log("QuerySelector 2 : ", document.querySelector(selector));
                     resolve(document.querySelector(selector));
                     observer.disconnect();
                 }
@@ -36,12 +34,11 @@
         });
     }
 
-    console.log("-------------------------We are connected-------------------------");
-
-    waitForElm('.community-points-summary').then((elm) => {
-        console.log('Element is ready');
-        console.log(elm);
-        var observerChest = new MutationObserver(function (mutations) {
+    function getChest(elm) {
+      if (document.querySelector('#numberOfChestGot') !== null) {
+          return;
+      }
+      var observerChest = new MutationObserver(function (mutations) {
             const button_div = document.querySelector(".claimable-bonus__icon");
             if(button_div != undefined) {
                 var button_to_click = button_div.parentNode.parentNode.parentNode;
@@ -56,7 +53,6 @@
             }
         });
         const number_chest_get_element = document.createElement('div');
-        const tw_chest_icon = '<div style="width: 2rem; height: 2rem;"><div class="ScIconLayout-sc-1bgeryd-0 kbOjdP tw-icon"><div class="ScAspectRatio-sc-1sw3lwy-1 dNNaBC tw-aspect"><div class="ScAspectSpacer-sc-1sw3lwy-0 gkBhyN"></div><svg width="100%" height="100%" version="1.1" viewBox="0 0 20 20" x="0px" y="0px" class="ScIconSVG-sc-1bgeryd-1 cMQeyU"><g><path fill-rule="evenodd" d="M16.503 3.257L18 7v11H2V7l1.497-3.743A2 2 0 015.354 2h9.292a2 2 0 011.857 1.257zM5.354 4h9.292l1.2 3H4.154l1.2-3zM4 9v7h12V9h-3v4H7V9H4zm7 0v2H9V9h2z" clip-rule="evenodd"></path></g></svg></div></div></div>';
         number_chest_get_element.innerHTML =
             '<div title="Nombre de coffres récupérés"'+
             'class="tw-mg-l-05 tw-core-button-label tw-core-button--secondary'+
@@ -65,5 +61,15 @@
             '><span class="tw-animated-number tw-semibold tw-mg-r-05" id="numberOfChestGot">' + number_chest_got + '</span>' + tw_chest_icon + '</div>';
         elm.append(number_chest_get_element);
         observerChest.observe(elm, {childList: true, attributes: true, subtree: true});
+    }
+
+    waitForElm('.community-points-summary').then((elm) => {
+        getChest(elm);
+        // Try to add a button.
+        let btnChestLaunch = document.createElement('div');
+        btnChestLaunch.innerHTML = tw_chest_icon;
+        document.querySelector('.top-nav__menu').lastChild.prepend(btnChestLaunch);
+        btnChestLaunch.addEventListener('click', () => { getChest(document.querySelector('.community-points-summary')) });
     });
+
 })();
