7c3e607e1639bb0c391a979dab8aab3d13bdf816
Sieo <Cosentino.hugo@gmail.com>
2025-04-03 19:00:01 +0200

    Update chest-auto-getter.js

--- chest-auto-getter.js
+++ chest-auto-getter.js
@@ -3,77 +3,106 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Let you get the chest automatically
-// @author       Hugo Mallaroni-Cosentino
+// @author       Sieo
 // @match      https://www.twitch.tv/*
 // @match      https://*.ext-twitch.tv/*
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
-(function() {
-   'use strict';
-   let number_chest_got = 0;
-   const tw_chest_icon = '<div style="width: 2rem; height: 2rem; fill: var(--color-text-base); margin-right: 0.5rem !important; cursor: pointer"><div class="tw-icon"><svg width="100%" height="100%" version="1.1" viewBox="0 0 20 20" x="0px" y="0px" class="ScIconSVG-sc-1bgeryd-1 cMQeyU"><g><path fill-rule="evenodd" d="M16.503 3.257L18 7v11H2V7l1.497-3.743A2 2 0 015.354 2h9.292a2 2 0 011.857 1.257zM5.354 4h9.292l1.2 3H4.154l1.2-3zM4 9v7h12V9h-3v4H7V9H4zm7 0v2H9V9h2z" clip-rule="evenodd"></path></g></svg></div></div>';
+(function () {
+    'use strict';
 
-   function waitForElm(selector) {
-       return new Promise(resolve => {
-           const existingElement = document.querySelector(selector);
-           if (existingElement) return resolve(existingElement);
+    let number_chest_got = 0;
+    let chestObserver = null; // Stocke l'observateur
+    let chestClicked = false; // Empêche le spam de clics
 
-           const observer = new MutationObserver(() => {
-               const element = document.querySelector(selector);
-               if (element) {
-                   resolve(element);
-                   observer.disconnect();
-               }
-           });
-           observer.observe(document.body, { childList: true, subtree: true });
-       });
-   }
+    // Icône du coffre
+    const tw_chest_icon = `
+        <div style="width: 2rem; height: 2rem; fill: var(--color-text-base); margin-right: 0.5rem !important; cursor: pointer">
+            <div class="tw-icon">
+                <svg width="100%" height="100%" viewBox="0 0 20 20">
+                    <g>
+                        <path fill-rule="evenodd" d="M16.503 3.257L18 7v11H2V7l1.497-3.743A2 2 0 015.354 2h9.292a2 2 0 011.857 1.257zM5.354 4h9.292l1.2 3H4.154l1.2-3zM4 9v7h12V9h-3v4H7V9H4zm7 0v2H9V9h2z" clip-rule="evenodd"></path>
+                    </g>
+                </svg>
+            </div>
+        </div>`;
 
-   function updateChestCount() {
-       const chestCounter = document.getElementById('numberOfChestGot');
-       if (chestCounter) {
-           chestCounter.innerText = ++number_chest_got;
-       }
-   }
+    // Fonction pour attendre un élément dans le DOM
+    function waitForElm(selector) {
+        return new Promise(resolve => {
+            if (document.querySelector(selector)) {
+                return resolve(document.querySelector(selector));
+            }
 
-   function checkAndClickChest() {
-       const button_div = document.querySelector(".claimable-bonus__icon");
-       if (button_div) {
-           const button_to_click = button_div.closest('button');
-           if (button_to_click) {
-               button_to_click.click();
-               updateChestCount();
-           }
-       }
-   }
+            const observer = new MutationObserver(mutations => {
+                if (document.querySelector(selector)) {
+                    resolve(document.querySelector(selector));
+                    observer.disconnect();
+                }
+            });
 
-   function ensureChestIcon(elm) {
-       let chestDisplay = document.getElementById('numberOfChestGot');
-       if (!chestDisplay) {
-           const chestContainer = document.createElement('div');
-           chestContainer.innerHTML = `
-               <div title="Nombre de coffres récupérés" class="tw-mg-l-05 tw-core-button-label tw-core-button--secondary tw-align-middle tw-border-radius-medium tw-align-items-center tw-flex tw-full-height">
-                   <span class="tw-animated-number tw-semibold tw-mg-r-05" id="numberOfChestGot">${number_chest_got}</span>
-                   ${tw_chest_icon}
-               </div>`;
-           elm.appendChild(chestContainer);
-       }
-   }
+            observer.observe(document.body, { childList: true, subtree: true });
+        });
+    }
 
-   function observeForChanges() {
-       const observer = new MutationObserver(() => {
-           ensureChestIcon(document.querySelector('.community-points-summary'));
-           checkAndClickChest();
-       });
+    // Met à jour le compteur de coffres récupérés
+    function updateChestCount() {
+        const chestCounter = document.getElementById('numberOfChestGot');
+        if (chestCounter) {
+            chestCounter.innerText = ++number_chest_got;
+        }
+    }
 
-       observer.observe(document.body, { childList: true, subtree: true });
-   }
+    // Vérifie et clique sur un coffre si présent
+    function checkAndClickChest() {
+        const button_div = document.querySelector(".claimable-bonus__icon");
+        if (button_div && !chestClicked) {
+            const button_to_click = button_div.closest('button');
+            if (button_to_click) {
+                chestClicked = true; // Empêche un double clic
+                button_to_click.click();
+                updateChestCount();
 
-   waitForElm('.community-points-summary').then((elm) => {
-       ensureChestIcon(elm);
-       checkAndClickChest();
-       observeForChanges();
-   });
+                setTimeout(() => { chestClicked = false; }, 500); // Réactive après un petit délai
+            }
+        }
+    }
+
+    // Assure que l'icône du coffre et le compteur sont affichés
+    function ensureChestIcon() {
+        const elm = document.querySelector('.community-points-summary');
+        if (!elm || document.getElementById('numberOfChestGot')) return;
+
+        const chestContainer = document.createElement('div');
+        chestContainer.innerHTML = `
+            <div title="Nombre de coffres récupérés"
+                class="tw-mg-l-05 tw-core-button-label tw-core-button--secondary
+                tw-align-middle tw-border-radius-medium tw-align-items-center tw-flex tw-full-height">
+                <span class="tw-animated-number tw-semibold tw-mg-r-05" id="numberOfChestGot">${number_chest_got}</span>
+                ${tw_chest_icon}
+            </div>`;
+        elm.appendChild(chestContainer);
+    }
+
+    // Observe les changements de DOM pour détecter les coffres
+    function observeForChanges() {
+        if (chestObserver) chestObserver.disconnect();
+
+        chestObserver = new MutationObserver(() => {
+            ensureChestIcon();
+            checkAndClickChest();
+        });
+
+        chestObserver.observe(document.body, { childList: true, subtree: true });
+    }
+
+    // Démarrage du script une fois que Twitch est chargé
+    waitForElm('.community-points-summary').then(() => {
+        ensureChestIcon();
+        checkAndClickChest();
+        observeForChanges();
+    });
 
 })();
+
